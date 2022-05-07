a4a15d15c3d98b890eac82438be311751cfdbc4c
jun <ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jp>
2022-05-07 16:42:49 +0900

    Change to use props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
+  const expense = [
+    { date: new Date(), title: "Buy toilet paper", amount: 12.34 },
+    { date: new Date(), title: "Buy new TV", amount: 222.34 },
+    { date: new Date(), title: "Car Insurance", amount: 302.04 },
+    { date: new Date(), title: "Buy a Building", amount: 34921 },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        date={expense[0].date}
+        title={expense[0].title}
+        amount={expense[0].amount}
+      />
+      <ExpenseItem
+        date={expense[1].date}
+        title={expense[1].title}
+        amount={expense[1].amount}
+      />
+      <ExpenseItem
+        date={expense[2].date}
+        title={expense[2].title}
+        amount={expense[2].amount}
+      />
+      <ExpenseItem
+        date={expense[3].date}
+        title={expense[3].title}
+        amount={expense[3].amount}
+      />
     </div>
   );
 }
