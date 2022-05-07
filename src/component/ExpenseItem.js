7182f47096552fbb674cac1f17256744a7c42995
jun <ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jp>
2022-05-07 17:49:14 +0900

    Change Date

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,9 +1,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const year = props.date.toLocaleString("ja-JP", { year: "numeric" });
+  const month = props.date.toLocaleString("ja-JP", { month: "long" });
+  const day = props.date.toLocaleString("ja-JP", { day: "numeric" });
+  const weekday = props.date.toLocaleString("ja-JP", { weekday: "long" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{year}</div>
+      <div>{month}</div>
+      <div>
+        {day} {weekday}
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
