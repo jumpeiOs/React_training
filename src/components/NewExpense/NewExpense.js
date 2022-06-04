e46c37f0dbcf51411f757cdc5d75e033db57b48b
jun <ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jp>
2022-06-04 18:42:53 +0900

    Make ChartBar

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [newExpense, setNewExpense] = useState(
+    <button type="submit">Add New Expense</button>
+  );
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setNewExpense(<button type="submit">Add New Expense</button>);
+  };
+
+  const newExpenseHandler = () => {
+    setNewExpense(<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />);
   };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense" onClick={newExpenseHandler}>
+      {newExpense}
     </div>
   );
 };
